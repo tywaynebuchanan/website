bdfc71905124a6cd82de78cad4f928a228157902
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2024-01-02 15:14:36 -0500

    completed faq page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 const btn = document.querySelector(".btn-mobile-nav");
+const addBtn = document.querySelectorAll(".add-outline");
+const faqText = document.querySelectorAll(".faq-text")
 const header = document.querySelector(".header");
 btn.addEventListener('click',()=>{
     header.classList.toggle("nav-open");
 })
+
+
+for (let i = 0; i < addBtn.length; i++) {
+  addBtn[i].addEventListener("click", function () {
+    faqText[i].classList.toggle("showAdd")
+  });
+}
