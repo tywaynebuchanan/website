7850671f746a5ce4ed5ced9f1837240cca077e3d
tywayneb <tywayneb@serha.gov.jm>
2024-01-03 13:39:57 -0500

    made the site

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 const btn = document.querySelector(".btn-mobile-nav");
 const addBtn = document.querySelectorAll(".add-outline");
 const faqText = document.querySelectorAll(".faq-text")
+const response = document.getElementById("message")
 const header = document.querySelector(".header");
+const form = document.getElementById("form")
+const subscribeMessage = document.getElementById("response")
+const subscribeForm = document.getElementById("subscribe-form")
 btn.addEventListener('click',()=>{
     header.classList.toggle("nav-open");
 })
@@ -12,3 +16,40 @@ for (let i = 0; i < addBtn.length; i++) {
     faqText[i].classList.toggle("showAdd")
   });
 }
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault();
+
+  let fullname = document.getElementById("fullname")
+  let email = document.getElementById("email")
+  let phone = document.getElementById("phonenumber")
+  let services = document.getElementById("services")
+  let message = document.getElementById("message")
+
+  if(fullname.value == "" || email.value == "" || phone.value == "" || services.value == "" || message == "")
+  {
+    response.innerHTML = "All fields are required"
+  }else{
+    fullname.value = ""
+    email.value = ""
+    phone.value = ""
+    services.value = ""
+    message.value = ""
+    response.innerHTML = "The form was submitted"
+  }
+
+})
+
+subscribeForm.addEventListener("submit",(e)=>{
+  e.preventDefault()
+
+  let email = document.getElementById("subscribe-email")
+
+  if(email.value == "")
+    {
+      subscribeMessage.innerHTML = "The email is required"
+    }else{
+      email.value = ""
+      subscribeMessage.innerHTML = "You have been subscribed to our news letter"
+    }
+})
